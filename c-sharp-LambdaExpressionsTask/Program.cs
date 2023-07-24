ece2826f6d69203b7ef96b714b3b2f12192aea90
omranali2111 <aljabri1234@hotmail.com>
2023-07-24 13:38:06 +0400

    Filter and display male students with a GPA greater than 3.5.

---
+++ c-sharp-LambdaExpressionsTask/Program.cs
@@ -0,0 +1,39 @@
+﻿using c_sharp_LambdaExpressionsTask;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Student> list = new List<Student>()
+        {
+            new Student("asad", 20, "Male", 3.8),
+            new Student("mlak", 22, "Female", 3.5),
+            new Student("ali", 21, "Male", 3.2),
+            new Student("Emily", 19, "Female", 3.9),
+            new Student("Ali", 24, "Male", 3),
+            new Student("mnar", 23, "Female", 3.6),
+            new Student("salim", 26, "Male", 3.7),
+
+         };
+
+        //disply all student from list
+        //foreach (var student in list)
+        //{
+        //    console.writeline(student);
+        //}
+
+        //Filter and display male students with a GPA greater than 3.5.
+
+        var maleGPA = list.Where(x => x.GPA > 3.5&&x.Gender=="Male");
+           
+
+        foreach (Student name in maleGPA)
+        {
+            Console.WriteLine(name);
+        }
+
+
+
+    }
+}
---
+++ c-sharp-LambdaExpressionsTask/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_LambdaExpressionsTask
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double GPA { get; set; }
+
+        public Student(string name, int age, string gender, double gpa)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+            GPA = gpa;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}, Gender: {Gender}, GPA: {GPA}";
+        }
+    }
+}
